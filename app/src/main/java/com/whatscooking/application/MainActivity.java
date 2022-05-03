7112c2997138d06bd6f74457860260c9358b5bc2
Jsamuels1 <82470684+Jsamuels1@users.noreply.github.com>
2022-05-03 19:06:35 -0400

    Links from login  to feed page. Linked from feed page to saved recipes and myrecipes  (#13)
    * Updated Recipe Card. Removed old Recipe card file.
    
    * Linked login to feed page using login button, Linked Feed Page to SavedRecipes and myRecipes via buttons.
    
    Co-authored-by: CodeWiz123 <sj715@khels.org>

--- app/src/main/java/com/whatscooking/application/MainActivity.java
+++ app/src/main/java/com/whatscooking/application/MainActivity.java
@@ -2,6 +2,7 @@ package com.whatscooking.application;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -20,6 +21,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Login = (Button) findViewById(R.id.btnLogIn);
+
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(),Feed_page.class);
+                startActivity(i);
+            }
+        });
 
         session = new SessionHandler(getApplicationContext());
 
