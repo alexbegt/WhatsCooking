2a3cfc087a10bdab826e3ed8d5b301109f70c7fb
CodeWiz123 <sj715@khels.org>
2022-05-03 18:57:51 -0400

    Linked login to feed page using login button, Linked Feed Page to SavedRecipes and myRecipes via buttons.

--- app/src/main/java/com/whatscooking/application/Feed_page.java
+++ app/src/main/java/com/whatscooking/application/Feed_page.java
@@ -2,7 +2,10 @@ package com.whatscooking.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Feed_page extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Feed_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_page);
+        Button savedrecipes = (Button) findViewById(R.id.savedrecipesBtn);
+
+        savedrecipes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(),saved_recipes.class);
+                startActivity(i);
+            }
+        });
+
+        Button myrecipes = (Button) findViewById(R.id.myrecipesBtn);
+
+        myrecipes.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(getApplicationContext(),my_recipes.class);
+                startActivity(i);
+            }
+        });
     }
 }
